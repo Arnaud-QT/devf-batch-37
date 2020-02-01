d6bc917dc5b738e4f81f0f1b5e444272444941af
Arnaud <arnaud.thevenet@quantrack.io>
2020-02-01 03:53:41 +0100

    Added React New Project

---
+++ Day9/my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/home/header';
+import Footer from './components/home/footer';
+
+function App() {
+  return (
+    <div className = "App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+
