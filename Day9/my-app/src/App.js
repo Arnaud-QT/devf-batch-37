8131367f6bb18f009207dd117e3856942f025c00
Arnaud <arnaud.thevenet@quantrack.io>
2020-02-08 03:50:05 +0100

    React-Router-Dom Commit

--- Day9/my-app/src/App.js
+++ Day9/my-app/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Header from './components/home/header';
 import Footer from './components/home/footer';
+import Pokemon from './components/home/Pokemon';
+import {BrowserRouter, Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Popper from 'popper.js';
+import $ from 'jquery';
 //import Login from './components/home/login';
       //<Login/>
 // import Button from './components/home/button';
      // <Button/>
-import Pokemon from './components/home/Pokemon';
 
 function App() {
   return (
-    <div className = "App">
-      <Header/>
- 
-      <Pokemon/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className = "App">
+        <Route path="/home" componente={Header}/>
+  
+        <Pokemon/>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- Day9/my-app/src/components/home/Pokemon.js
+++ Day9/my-app/src/components/home/Pokemon.js
@@ -10,10 +10,11 @@ function Pokemon(params) {
     }, [])
 
     const getPokemones = () =>{
-        axios.get("https://pokeapi.co/api/v2/pokemon/")
+        axios.get("https://incubator-devf.herokuapp.com/api/v1/get/all/active")
         .then(result => {
-            const {results} = result.data;
-            setPokemones(results);
+            console.log(result);
+            const {data} = result;
+            setPokemones(data);
         })
         .catch(err => {
             console.log(err);
@@ -23,7 +24,14 @@ function Pokemon(params) {
     const showPokemones = () => {
         if (pokemones.length > 0) {
             return pokemones.map((elem, index)=>{
-                return <li key={index}>{elem.name}</li>;
+                return (<div className="card">
+                <div className="card-body">
+                <h5 className="card-title">Name: {elem.name}</h5>
+                
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>);
             })
         }
     }
